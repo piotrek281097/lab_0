a85fc1ad896f8cbb361a0db35a9de020764f7de1
radoslaw.adamus <radoslaw.adamus@p.lodz.pl>
2019-03-06 17:45:31 +0100

    new tests added

--- src/test/java/edu/iis/mto/lab_0/TailsTest.java
+++ src/test/java/edu/iis/mto/lab_0/TailsTest.java
@@ -1,29 +1,36 @@
 package edu.iis.mto.lab_0;
 
-import static org.junit.Assert.assertThat;
-
 import java.util.List;
 
 import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.empty;
+import static org.junit.Assert.assertThat;
+
 public class TailsTest {
 
     private static final String HELLO = "Hello";
+    private static final Object FOURTH_ELEM_VALUE = "LO";
+    private static final int FOURTH_ELEM_INDEX = 3;
     private TailsGenerator tailsGenerator;
     private List<String> tails;
 
     @Before
     public void setup() {
-
         tailsGenerator = new TailsGenerator();
         tails = tailsGenerator.tails(HELLO);
     }
 
     @Test
-    public void tailsShouldReturnListOfStringsOfSizeEqualsToInputStringLengthPlusOne() {
+    public void tailsShouldReturnEmptyListIfParameterIsNull() {
+        tails = tailsGenerator.tails(null);
+        assertThat(tails, empty());
+    }
 
+    @Test
+    public void tailsShouldReturnListOfStringsOfSizeEqualsToInputStringLengthPlusOne() {
         assertThat(tails, Matchers.hasSize(HELLO.length() + 1));
     }
 
@@ -34,8 +41,12 @@ public class TailsTest {
     }
 
     @Test
-    public void tailsShouldReturnEmptyStringAsLastElement() {
+    public void tailsShouldReturn_LO_AsFourthString() {
+        assertThat(tails.get(FOURTH_ELEM_INDEX), Matchers.equalTo(FOURTH_ELEM_VALUE));
+    }
 
+    @Test
+    public void tailsShouldReturnEmptyStringAsLastElement() {
         assertThat(tails.get(tails.size() - 1), Matchers.equalTo(""));
     }
 
